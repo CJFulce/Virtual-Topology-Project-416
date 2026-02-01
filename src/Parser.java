291afeb1680c5e0d151617fe5e556e4953632d03
CJFulce <conner.fulciniti@bsu.edu>
2026-02-01 11:16:06 -0500

    getNeighborAddr() added to see device neighbors for port creation

--- src/Parser.java
+++ src/Parser.java
@@ -35,44 +35,68 @@ public class Parser {
         try (Scanner configScanner = new Scanner(this.configFile)) {
             while (configScanner.hasNextLine()) {
                 String rawConfig = configScanner.nextLine();
-                if(rawConfig.contains(deviceId)){
+                if(rawConfig.contains(deviceId) && rawConfig.startsWith("device")){
                     String[] lineElements = rawConfig.split(" ");
                         int parsedPortNumber = Integer.parseInt(lineElements[3]);
                         System.out.println("Gotcha");
                         return parsedPortNumber;
-                }else{
-                    System.out.println("Nothing here");
                 }
-            
+                // else{
+                //     System.out.println("Nothing here for port");
+                // }
             }
         } catch (FileNotFoundException error) {
             System.err.println(error);
             error.printStackTrace();
         }
         return 0;
-     
     }
     private String getDeviceIP(String deviceId){
     
         try (Scanner configScanner = new Scanner(this.configFile)) {
             while (configScanner.hasNextLine()) {
                 String rawConfig = configScanner.nextLine();
-                if(rawConfig.contains(deviceId)){
+                if(rawConfig.contains(deviceId) && rawConfig.startsWith("device")){
                     String[] lineElements = rawConfig.split(" ");
                         String parsedIPAddress = lineElements[2];
                         System.out.println("Got IP");
                         return parsedIPAddress;
-                }else{
-                    System.out.println("Nothing here for IP");
                 }
-            
+                //     else{
+                // //     System.out.println("Nothing here for IP");
+                // }
             }
         } catch (FileNotFoundException error) {
             System.err.println(error);
             error.printStackTrace();
         }
         return "0.0.0.0";
-     
+    }
+
+    private List<String> getNeighborAddr(String deviceId){
+        List<String> neighbors = new ArrayList<>();
+        try (Scanner configScanner = new Scanner(this.configFile)) {
+            while (configScanner.hasNextLine()) {
+                String rawConfig = configScanner.nextLine();
+                if(rawConfig.startsWith("link")){
+                    String[] lineElements = rawConfig.split(" ");
+                    // use equals to compare strings
+                    if(lineElements[1].equals(deviceId)){
+                        // add the neighbor id or ip (adjust index per your file format)
+                        neighbors.add(lineElements[2]);
+                    }if(lineElements[2].equals(deviceId)){
+                        neighbors.add(lineElements[1]);
+                    }
+                }
+
+            }
+            return neighbors;
+
+        } catch (FileNotFoundException error) {
+            System.err.println(error);
+            error.printStackTrace();
+        }
+        return neighbors; //will return empty if not seen in config file
     }
 public static void main(String[] args){
 
@@ -95,7 +119,18 @@ public static void main(String[] args){
     if(ipAddress == "0.0.0.0\n"){
         System.out.println("Error loading IP Address");
     }else{
-        System.out.printf("IP Address: %s", ipAddress);
+        System.out.printf("IP Address: %s\n", ipAddress);
+    }
+
+    List<String> neighborList = testParser.getNeighborAddr("S2");
+    
+    if(neighborList.isEmpty()){
+        System.out.println("No neighbors found");
+    }else{
+        for(String neighbor : neighborList){
+            System.out.printf("Neighbor: %s\n", neighbor);
+        }
+        
     }
     }
 
